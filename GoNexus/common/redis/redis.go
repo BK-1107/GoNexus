61296e7c8a7a29e38dc7bdd813ea702d5cd73638
Novarisea <1129271075awm@gmail.com>
2026-02-15 15:36:24 +0900

    feat: add Redis client and key conventions

---
+++ GoNexus/common/redis/redis.go
@@ -0,0 +1,68 @@
+package redis
+
+import (
+	"GoNexus/config"
+	"context"
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	redisCli "github.com/redis/go-redis/v9"
+)
+
+var Rdb *redisCli.Client
+
+var ctx = context.Background()
+
+func Init() {
+	conf := config.GetConfig()
+	host := conf.RedisConfig.RedisHost
+	port := conf.RedisConfig.RedisPort
+	password := conf.RedisConfig.RedisPassword
+	db := conf.RedisDb
+	addr := host + ":" + strconv.Itoa(port)
+
+	Rdb = redisCli.NewClient(&redisCli.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       db,
+		Protocol: 2, // ?? Protocol 2 ?? maint_notifications ??
+	})
+
+}
+
+func SetCaptchaForEmail(email, captcha string) error {
+	key := GenerateCaptcha(email)
+	expire := 2 * time.Minute
+	return Rdb.Set(ctx, key, captcha, expire).Err()
+}
+
+func CheckCaptchaForEmail(email, userInput string) (bool, error) {
+	key := GenerateCaptcha(email)
+
+	storedCaptcha, err := Rdb.Get(ctx, key).Result()
+	if err != nil {
+		if err == redisCli.Nil {
+
+			return false, nil
+		}
+
+		return false, err
+	}
+
+	if strings.EqualFold(storedCaptcha, userInput) {
+
+		// ??????? key
+		if err := Rdb.Del(ctx, key).Err(); err != nil {
+
+		} else {
+
+		}
+		return true, nil
+	}
+
+	return false, nil
+}
+
+
