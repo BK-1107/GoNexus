608e9c3603e41f8e93e5c18483fb8bd6c0f0b7fc
Novarisea <1129271075awm@gmail.com>
2026-06-13 05:44:01 +0900

    feat: support knowledge document uploads

---
+++ GoNexus/controller/file/file.go
@@ -0,0 +1,58 @@
+package file
+
+import (
+	"GoNexus/common/code"
+	"GoNexus/controller"
+	"GoNexus/service/file"
+	"log"
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+// FilePath 返回服务端保存后的文件路径，Response 负责统一状态码和提示信息。
+type UploadFileResponse struct {
+	FilePath string `json:"file_path,omitempty"`
+	controller.Response
+}
+
+// 读http取传来的上传的文件
+func UploadRagFile(c *gin.Context) {
+	res := new(UploadFileResponse)
+
+	// 读取上传文件，把文件实体抽出来
+	uploadedFile, err := c.FormFile("file")
+	if err != nil {
+		log.Println("FormFile fail ", err)
+		c.JSON(http.StatusOK, res.CodeOf(code.CodeInvalidParams))
+		return
+	}
+
+	// 从上下文环境里，把解密后的纯文字名字拿出来
+	username := c.GetString("userName")
+	if username == "" {
+		log.Println("Username not found in context")
+		c.JSON(http.StatusOK, res.CodeOf(code.CodeInvalidToken))
+		return
+	}
+
+	// 转移service 层，完成文件类型校验、保存文件，以及文本类文件的 RAG 索引构建。
+	filePath, err := file.UploadRagFile(username, uploadedFile)
+	if err != nil {
+		log.Println("UploadFile fail ", err)
+		if strings.Contains(err.Error(), "RAG upload only supports") || strings.Contains(err.Error(), "file is empty") {
+			res.CodeOf(code.CodeInvalidParams)
+			res.StatusMsg = err.Error()
+			c.JSON(http.StatusOK, res)
+			return
+		}
+		c.JSON(http.StatusOK, res.CodeOf(code.CodeServerBusy))
+		return
+	}
+	// 返回文件路径给前端，gin不需要return。
+	res.Success()
+	res.FilePath = filePath
+	c.JSON(http.StatusOK, res)
+}
+
